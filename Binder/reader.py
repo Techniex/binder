21bd8ffc5015cc3ffa03ace172c8fe25f6774821
Techniex <purnendu@techniex.com>
2020-06-06 09:05:43 +0530

    Updated Reader.py (read_text) with simple functions to be implemented in fileutil.py.

--- Binder/reader.py
+++ Binder/reader.py
@@ -1,139 +1,125 @@
 # -*- coding: utf-8 -*-
 """
-author      = Purnendu Kumar <purnendu@techniex.com>
-copyright   = Copyright (C) 2020 Techniex <https://techniex.com>
-license     = GPLv3
-version     = 1.0
-url         = https://github.com/Techniex/binder
+author            = Purnendu Kumar <purnendu@techniex.com>
+copyright     = Copyright (C) 2020 Techniex <https://techniex.com>
+license         = GPLv3
+version         = 1.0
+url                 = https://github.com/Techniex/binder
 """
-import os
-from . import param
-from . import fileutil
 import cv2
+from . import fileutil
+from . import param
 
 class Reader(fileutil.FileUtil):
-  def __init__(self):
-    super().__init__()
-    
-  def readTxt(self, filepath, **kwargs):
     """
     Description
     -----------
-      reads the text based files and returns the output as dict. Keys of dict depends on parameters passed.
-      default keys are: filename and lines as list of lines
-      supported file types are: .txt, .c, .py, .m, .cpp, .h, .csv, .tab, etc.(any other plain text based file with utf encoding)
-      
-    Input Parameter
+    Class contains methods to read text, image or binary(raw) file.
+
+    Public Methods
     ---------------
-    filepath: Path of text based file to be read.
-    kwargs = dictionary with keys     
-      skiplines: integer to identify how many lines to skip and it won't be included in header or data, default = 0
-      headerlines: number of lines in header. if the keyword is not defined program doesn't look for header in file.
-      skipwhitelines: if True, skips all white lines or lines only with comment identifier are skipped, default True
+    read_text(filepath, **kwargs)
 
-      commentidentifier: character to be identified as starting of comment. e.g. "#" for configfiles
-      headercommenthandler: 'skipcomment'/ 'uncomment'
-      datacommenthandler: 'skipcomment'/ 'uncomment'
-      removeinlinecomment: True/False
-
-      headerfilter: select lines only if starts with keyword
-      datafilter: select lines only if starts with keyword
-
-      headerdelimeter: to seperate key and value pair in header. if not defined, headers are returned as lines.
-      headerformat:
-
-      datadelimeter:
-      dataformat:
-
-    Input Parameter Type
-    --------------------
-    filepath: string --> required
-    kwargs['skiplines']: int --> optional
-    kwargs['commentidentifier']: str --> optional
-    kwargs['skipwhitelines']: bool --> optional
-    kwargs['header']: bool --> optional
-    kwargs['headerlines']: int --> optional
-    kwargs['headerdict']: bool --> optional
-    kwargs['headerdelimeter']: str --> optional
-    kwargs['commentskip']: bool --> optional
-    kwargs['startswithfilter']: str/list --> optional
-    kwargs['startswithexclude']: str/list --> optional
-    kwargs['linefilter']: str/list --> optional
-    kwargs['lineexclude']: str/list --> optional
-    kwargs['columns']: bool --> optional
-    kwargs['delimeter']: str --> optional
-    
-    Returns
-    -------
-    dictionary with default keywords 'filename', 'filetype', 'lines', remaining keys depends on file and selection of arguments.
-    
-    Return Type
-    -----------
-    dict
-  
-    To Do
-    -----
-    1. Create Documentation
-    2. Implement the function in c for faster operations.
-    
+    Private Methods
+    ---------------
+    __init__()
     """
-    if self.verbose:
-      print("Function call: readTxt(args)")
+    def __init__(self, verbose=False):
+        super().__init__(verbose)
 
-    #initialize return
-    rdict = {'Error':self.error, 'Warning':self.warning}
+    def read_text(self, filepath, **kwargs):
+        """
+        Description
+        -----------
+            reads the text based files and returns the output as dict.
+            supported file types are: .txt, .c, .py, .m, .cpp, .h, .csv, .tab, etc.
+            (any other plain text based file with utf encoding)
 
-    # Load default kwargs if not provided by user
-    kwargs = self.__loadkwargs__(filepath, 'text', kwargs)
+        Arguments
+        ---------
+            filepath(raw str): Path of text based file to be read.
+            kwargs(optional key value pair)
+                kwargs['skip_top'](positive int/str) :
+                    number of lines to be skipped from top. default = 0
+                    till the line where the str is in line is stripped from top.
+                kwargs['skip_bottom'](positive int) :
+                    number of lines to be skipped from top. default = 0
+                    first line from bottom with occurance of specified str
+                kwargs['comment_handle'](dict):
+                    parameters required to operate on comments.
+                        comment_handle['comment_method'](str): default = 'none'
+                            options = 'none', 'skip', 'uncomment'
+                        comment_handle['lcid'](str/tuple) :
+                            line comment identifier. default = '#'
+                        comment_handle['mlcid](list) :
+                            multiline comment identifier [start, stop] default = []
+                        comment_handle['docstring_method'](str): default = 'none'
+                            options = 'none', 'skip', 'read'
+                        comment_handle['ldocid'](str/tuple) :
+                            one line docstring identifier. default = "#!"
+                        comment_handle['mldocid'](list) :
+                            one line docstring identifier. default = ['"""' , '"""']
+                        comment_handle['skipwhitelines'](bool): default = False
+                kwargs['filters'](str/list):
+                    filter to choose/reject lines from selected section of file. default = []
+                kwargs['data_format'](str):
+                    output formatting. default = 'line'
+                    options = 'line', 'list', 'dict', 'list', 'vlist', 'vdict'
+                kwargs['delimeter'](str/list):
+                    delimeter to sort header data. default = [',']
 
-    # Read file get all lines in list
-    with open(filepath , 'r') as infile:
-      origlines = infile.read().splitlines()
-    
-    #skiplines
-    origlines = origlines[kwargs['skiplines']:]
-    
-    # separate header, data, footer
-    outfile = {}
-    outfile['header'] = origlines[0: kwargs['headerlines']]
-    outfile['data'] = origlines[kwargs['headerlines']:len(origlines)-kwargs['footerlines']]
-    outfile['footer'] = origlines[kwargs['footerlines']:]
-    
-    #comment handler
-    if kwargs['comment_maipulation']:
-      for key in outfile.keys():
-        outfile[key] = self.comment_handler(outfile[key],kwargs[key+'_comment_handle'])[0]
+        Returns
+        -------
+        dict with keys: 'error'(int) , 'warning'(int)
+        , 'data'(formatted data), 'docstring(dict)', 'data_format'(str).
+        """
 
-    #Handle docstring
-    docstring = {}
-    if kwargs['read_docstring']:
-      for key in outfile.keys():
-        outval = self.comment_handler(outfile[key],kwargs[key+'_docstring_handle'])[0]
-        outfile[key] = outval[0]
-        docstring[key] = outval[1]
+        if self.verbose:
+            print("Function call: readTxt(args)")
 
-    # filter data
-    for key in outfile.keys():
-      outfile[key] = self.str_filter(outfile[key], kwargs[key+'_filter'])
-          
-    #format data
-    dataformat = {}
-    for key in outfile.keys():
-      data = self.line_format(outfile[key], kwargs[key+'_format'], kwargs[key+'_delimeter'], kwargs[key+"_comment_handle"])
-      outfile[key] = data[1]
-      dataformat[key] = data[0]
-    
-    rdict.update(dataformat)
-    rdict.update(outfile)
-    rdict.update(docstring)
-    return rdict
-         
-  def readImg(self, filepath, **kwargs):
-    return cv2.imread(filepath, -1)
+        #initialize return
+        rdict = {'error':self.error, 'warning':self.warning}
 
-  def readRaw(self, filepath, **kwargs):
-    with open(filepath , 'rb') as rawfile:
-      xs= bytearray(b'')
-      for line in rawfile:
-        xs = xs + line
-    return xs
+        # Load default kwargs if not provided by user
+        kwargs = self.__loadkwargs__(filepath, 'text', kwargs)
+
+        # Read file get all lines in list
+        with open(filepath, 'r') as infile:
+            original_lines = infile.read().splitlines()
+
+        # get data
+        selected_lines = original_lines[kwargs['skip_top']:\
+            len(original_lines)-kwargs['skip_bottom']]
+
+        #comment handler
+        [stripped_lines, docstring] = self.comment_handler(selected_lines,\
+             kwargs['comment_handle'])[0]
+
+        # filter data
+        filtered_lines = self.str_filter(stripped_lines, kwargs['filters'])
+
+        #format data
+        [data_format, formatted_data] = self.line_format(filtered_lines,\
+             kwargs['data_format'], kwargs['delimeter'], kwargs["comment_handle"])
+
+        rdict['data'] = formatted_data
+        rdict['docstring'] = docstring
+        rdict['data_format'] = data_format
+        return rdict
+
+    # TODO : image read from binary
+    def read_image(self, filepath):
+        """
+        Read image
+        """
+        return cv2.imread(filepath, -1)
+
+    def read_raw(self, filepath):
+        """
+        Read Binary
+        """
+        with open(filepath, 'rb') as rawfile:
+            xsap = bytearray(b'')
+            for line in rawfile:
+                xsap = xsap + line
+        return xsap
